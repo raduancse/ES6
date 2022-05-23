c17733e641ee3b6477962cbc9be42c5feff21572
Md. Raduan Islam <raduan.cse@gmail.com>
2022-05-24 00:45:18 +0600

    ES6 practise

---
+++ bangladesh.js
@@ -0,0 +1,6 @@
+import {laptop,phone,cosmetic} from "./china.js";
+
+
+console.log(laptop);
+phone();
+cosmetic();
---
+++ china.js
@@ -0,0 +1,10 @@
+const laptop = 'Hp Laptop, Asus laptop';
+
+function phone(){
+    console.log("mobile phn from china")
+}
+function cosmetic(){
+    console.log("cosmetic from Iran")
+}
+
+export {laptop,phone,cosmetic};
